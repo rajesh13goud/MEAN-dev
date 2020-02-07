7aae8f2dbce56690b6f76d04a6ce59f784e3b4a5
rajesh13goud <rajeshdgoud@gmail.com>
2020-02-07 15:10:36 +0000

    let and const

--- main.js
+++ main.js
@@ -10,13 +10,50 @@
 // }
 // findNemo(large);
 
-const EventEmitter = require('events');
+// const EventEmitter = require('events');
 
-const emitter = new EventEmitter();
+// const emitter = new EventEmitter();
 
-//Register an Event
-emitter.on('messageLogged', function(){
-  console.log('Listner Called')
-})
-//Raise an event
-emitter.emit('messageLogged');
+// //Register an Event
+// emitter.on('messageLogged', function(){
+//   console.log('Listner Called')
+// })
+// //Raise an event
+// emitter.emit('messageLogged');
+
+//ES5
+
+var firstName5 = 'Rajesh';
+var age5 = 28;
+firstName5 = 'Raja';
+
+console.log(firstName5);
+
+//ES6
+
+const firstName6 = 'Rajesh mama';
+let age6 = 28;
+// firstName6 = 'Rajesh RAJA';
+
+console.log(firstName6);
+
+//ES5 function-scoped
+
+function driverLicense5(passedTest){
+  if(passedTest){
+    var name5 = 'Rajesh';
+    var yearOfBirth = 1990;
+  }
+    console.log(name5+ ' '+yearOfBirth);
+}
+driverLicense5(true);
+
+//ES6 Block-scoped
+function driverLicense6(passedTest){
+  if(passedTest){
+    let name6 = 'Rajesh';
+    const yearOfBirth = 1990;
+  }
+    console.log(name6+ ' '+yearOfBirth);
+}
+driverLicense6(true);
